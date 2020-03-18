1a5662562cdb4c2d4caabd076c23a1ea091a4cb4
Jacob Valdemar <jacobvaldemarand@gmail.com>
2020-03-18 17:11:13 +0100

    More tests for statiuonControl

--- Ladeskab/Ladeskab.Unit.Test/StationControlUnitTest.cs
+++ Ladeskab/Ladeskab.Unit.Test/StationControlUnitTest.cs
@@ -27,14 +27,21 @@ namespace Ladeskab.Unit.Test
             _uut = new StationControl(_fakeRfidReader, _fakeDoor, _fakeChargeControl,_fakeDisplay,_fakeLogger);
         }
 
+        #region RfidDetected
+
+
+        #region LadeskabIsAvailable
+
+        #region DeviceIsConnected
+
+
         // RfidDetected_LadeskabIsAvailable_DeviceIsConnected
         // RfidDetected_LadeskabIsAvailable_DeviceIsConnected_LockDoor_Called
         // RfidDetected_LadeskabIsAvailable_DeviceIsConnected_StartCharge_Called
         // RfidDetected_LadeskabIsAvailable_DeviceIsConnected_DisplayStationMessage_Called
         // RfidDetected_LadeskabIsAvailable_DeviceIsConnected_LogDoorLocked_Called
 
-        [Test]
-        public void RfidDetected_LadeskabIsAvailable_DeviceIsConnected_LockDoor_Called()
+        public int RfidDetected_LadeskabIsAvailable_DeviceIsConnected()
         {
             // Arrange
             int testId = 3;
@@ -42,7 +49,14 @@ namespace Ladeskab.Unit.Test
             // Act
             _fakeChargeControl.IsConnected().ReturnsForAnyArgs(true);
             _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
+            
+            return testId;
+        }
 
+        [Test]
+        public void RfidDetected_LadeskabIsAvailable_DeviceIsConnected_LockDoor_Called()
+        {
+            RfidDetected_LadeskabIsAvailable_DeviceIsConnected();
 
             // Assert
             _fakeDoor.Received(1).LockDoor();
@@ -51,13 +65,7 @@ namespace Ladeskab.Unit.Test
         [Test]
         public void RfidDetected_LadeskabIsAvailable_DeviceIsConnected_StartCharge_Called()
         {
-            // Arrange
-            int testId = 3;
-
-            // Act
-            _fakeChargeControl.IsConnected().ReturnsForAnyArgs(true);
-            _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
-
+            RfidDetected_LadeskabIsAvailable_DeviceIsConnected();
 
             // Assert
             _fakeChargeControl.Received(1).StartCharge();
@@ -66,13 +74,7 @@ namespace Ladeskab.Unit.Test
         [Test]
         public void RfidDetected_LadeskabIsAvailable_DeviceIsConnected_DisplayStationMessage_Called()
         {
-            // Arrange
-            int testId = 3;
-
-            // Act
-            _fakeChargeControl.IsConnected().ReturnsForAnyArgs(true);
-            _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
-
+            RfidDetected_LadeskabIsAvailable_DeviceIsConnected();
 
             // Assert
             _fakeDisplay.Received(1).StationMessage(Arg.Is<string>(s => s.Contains("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.")));
@@ -81,17 +83,15 @@ namespace Ladeskab.Unit.Test
         [Test]
         public void RfidDetected_LadeskabIsAvailable_DeviceIsConnected_LogDoorLocked_Called()
         {
-            // Arrange
-            int testId = 3;
-
-            // Act
-            _fakeChargeControl.IsConnected().ReturnsForAnyArgs(true);
-            _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
-
+            int testId = RfidDetected_LadeskabIsAvailable_DeviceIsConnected();
 
             // Assert
             _fakeLogger.Received(1).LogDoorLocked(testId);
         }
+
+        #endregion
+
+        #region DeviceIsNotConnected
 
         // RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected
         // RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_LockDoor_NotCalled
@@ -99,8 +99,7 @@ namespace Ladeskab.Unit.Test
         // RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_DisplayStationMessage_Called
         // RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_LogDoorLocked_NotCalled
 
-        [Test]
-        public void RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_LockDoor_NotCalled()
+        public int RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected()
         {
             // Arrange
             int testId = 3;
@@ -109,55 +108,108 @@ namespace Ladeskab.Unit.Test
             _fakeChargeControl.IsConnected().ReturnsForAnyArgs(false);
             _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
 
+            return testId;
+        }
+
+        [Test]
+        public void RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_LockDoor_NotCalled()
+        {
+            RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected();
 
             // Assert
-            _fakeDoor.Received(1).LockDoor();
+            _fakeDoor.DidNotReceive().LockDoor();
         }
 
         [Test]
         public void RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_StartCharge_NotCalled()
         {
-            // Arrange
-            int testId = 3;
-
-            // Act
-            _fakeChargeControl.IsConnected().ReturnsForAnyArgs(false);
-            _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
-
+            RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected();
 
             // Assert
-            _fakeChargeControl.Received(1).StartCharge();
+            _fakeChargeControl.DidNotReceive().StartCharge();
         }
 
         [Test]
         public void RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_DisplayStationMessage_Called()
         {
-            // Arrange
-            int testId = 3;
-
-            // Act
-            _fakeChargeControl.IsConnected().ReturnsForAnyArgs(false);
-            _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
-
+            RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected();
 
             // Assert
-            _fakeDisplay.Received(1).StationMessage(Arg.Is<string>(s => s.Contains("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.")));
+            _fakeDisplay.Received(1).StationMessage(Arg.Is<string>(s => s.Contains("Din telefon er ikke ordentlig tilsluttet. Prøv igen.")));
         }
 
         [Test]
         public void RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected_LogDoorLocked_NotCalled()
         {
-            // Arrange
-            int testId = 3;
-
-            // Act
-            _fakeChargeControl.IsConnected().ReturnsForAnyArgs(false);
-            _fakeRfidReader.RfidReadEvent += Raise.EventWith(_fakeRfidReader, new RfidReadEventArgs(testId));
-
+            int testId = RfidDetected_LadeskabIsAvailable_DeviceIsNotConnected();
 
             // Assert
-            _fakeLogger.Received(1).LogDoorLocked(testId);
+            _fakeLogger.DidNotReceive().LogDoorLocked(testId);
         }
+
+        #endregion
+
+        #endregion
+
+
+        #region DoorOpen
+
+        // RfidDetected_DoorOpen
+        // RfidDetected_DoorOpen_LockDoor_NotCalled
+        // RfidDetected_DoorOpen_StartCharge_NotCalled
+        // RfidDetected_DoorOpen_DisplayStationMessage_NotCalled
+        // RfidDetected_DoorOpen_LogDoorLocked_NotCalled
+
+        public void RfidDetected_DoorOpen()
+        {
+            // Arrange
+
+            // Act
+            _fakeDoor.OpenDoor();
+        }
+
+        [Test]
+        public void RfidDetected_DoorOpen_LockDoor_NotCalled()
+        {
+            RfidDetected_DoorOpen();
+
+            // Assert
+            _fakeDoor.DidNotReceive().LockDoor();
+        }
+
+        [Test]
+        public void RfidDetected_DoorOpen_StartCharge_NotCalled()
+        {
+            RfidDetected_DoorOpen();
+
+            // Assert
+            _fakeChargeControl.DidNotReceive().StartCharge();
+        }
+
+        [Test]
+        public void RfidDetected_DoorOpen_DisplayStationMessage_NotCalled()
+        {
+            RfidDetected_DoorOpen();
+
+            // Assert
+            _fakeDisplay.DidNotReceive().StationMessage("d");
+        }
+
+        [Test]
+        public void RfidDetected_DoorOpen_LogDoorLocked_NotCalled()
+        {
+            RfidDetected_DoorOpen();
+
+            // Assert
+            _fakeLogger.DidNotReceiveWithAnyArgs().LogDoorLocked(1);
+        }
+
+        #endregion
+
+
+
+
+        #endregion
 
 
         #region DoorEvents
