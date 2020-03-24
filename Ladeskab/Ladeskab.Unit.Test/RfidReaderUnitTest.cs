9cfb688c22580c91cae84b97bf7bb7dd114b3579
Anders Tøgersen <54847232+au563513@users.noreply.github.com>
2020-03-24 11:42:57 +0100

    RfidReader Unit Test done with 100% coverage

---
+++ Ladeskab/Ladeskab.Unit.Test/RfidReaderUnitTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture]
+    class RfidReaderUnitTest
+    {
+        private RfidReader _uut;
+        private RfidReadEventArgs _receivedRfidReadEventArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _receivedRfidReadEventArgs = null;
+            _uut = new RfidReader();
+
+            _uut.RfidReadEvent += (o, args) => { _receivedRfidReadEventArgs = args; };
+        }
+
+        [TestCase(int.MaxValue)]
+        [TestCase(1)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(int.MinValue)]
+        public void ReadRfid_RfidReadEventFired(int id)
+        {
+            _uut.ReadRfid(id);
+
+            Assert.That(_receivedRfidReadEventArgs, Is.Not.Null);
+        }
+
+        [TestCase(int.MaxValue)]
+        [TestCase(1)]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(int.MinValue)]
+        public void ReadRfid_RfidReadEvent_WithRightArgs(int id)
+        {   
+            _uut.ReadRfid(id);
+
+            Assert.That(_receivedRfidReadEventArgs.Id, Is.EqualTo(id));
+        }
+    }
+}
